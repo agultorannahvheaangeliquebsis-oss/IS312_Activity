e0ede6b9b36dfe751663a5b827a1b3f0198a55bd
agultorannahvheaangeliquebsis-oss <agulto.rannahvheaangelique.bsis@gmail.com>
2026-03-02 09:47:58 +0800

    Add files via upload

---
+++ Agulto_MultiplyTwoNum.py
@@ -0,0 +1,10 @@
+print(" ________________________________________ ")
+print("|          Multiply Two Numbers          |")
+print("|________________________________________|")
+
+agultoFirstNum = int(input("Enter first number: "))
+agultoSecondNum = int(input("Enter second number: "))
+
+agultoAns = agultoFirstNum * agultoSecondNum
+
+print(f"Answer: {agultoAns}")
