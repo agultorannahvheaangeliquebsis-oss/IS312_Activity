216ed92ebeeefd68e9ffba76e4aa9ee09e29fdd2
agultorannahvheaangeliquebsis-oss <agulto.rannahvheaangelique.bsis@gmail.com>
2026-03-02 09:40:35 +0800

    Add files via upload

---
+++ Agulto_AreaOfCircle.py
@@ -0,0 +1,10 @@
+print(" ________________________________________ ")
+print("|      Calculate the Area of Circle      |")
+print("|________________________________________|")
+
+agultoRadius = int(input("Enter radius: "))
+agultoArea = 3.14 * (agultoRadius ** 2)
+
+print(f"The approximate area is: {round(agultoArea)}")
+
+
